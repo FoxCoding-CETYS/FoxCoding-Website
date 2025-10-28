4dc0d8429e1d1235eb3e9ca2f30f637c49895a03
EmilioFex20 <emilio.pouget@cetys.edu.mx>
2025-10-27 23:41:56 -0700

    feat(setup): initialize Next.js project with Tailwind, TypeScript and Shadcn
    - Set up Next.js 14+ with TypeScript
    - Install and configure Tailwind CSS
    - Configure Inter font
    - Create base project structure
    - Set ThemeProvider to enable the dark mode feature
    
    Closes #1

---
+++ foxcoding/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Inter } from 'next/font/google'
+import { Header } from "@/components/layout/header";
+import "./globals.css";
+import { ThemeProvider } from "next-themes";
+
+const inter = Inter({
+  display: "swap",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "FoxCoding",
+  description: "Promoting interest in computing and information technologies among members of the Cetys community",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>
+        <ThemeProvider attribute="class" defaultTheme="light">
+          <Header />
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
