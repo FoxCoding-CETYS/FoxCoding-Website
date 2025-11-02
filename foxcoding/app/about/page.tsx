7e77e5c933b398f7a1b8fc55efc0b1194f9412a3
LestathRiveraD <cesarrivera0404@gmail.com>
2025-11-02 15:33:33 -0800

    Mision-vision section of the about us page

--- foxcoding/app/about/page.tsx
+++ foxcoding/app/about/page.tsx
@@ -1,9 +1,11 @@
 import { HeroSection } from "@/components/sections/heroSection";
+import { MisionVision } from "@/components/sections/mision-vision";
 
 export default function About() {
     return (
         <>
             <HeroSection first_word={"About"} second_word={"Foxcoding"} />
+            <MisionVision />
         </>
     );
 }
